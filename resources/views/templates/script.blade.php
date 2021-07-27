48616ca45c1e04fbaa4583183c6c925800941077
rayhan256 <rayhangamawanto@gmail.com>
2021-07-27 17:04:46 +0700

    added secure assets

--- resources/views/templates/script.blade.php
+++ resources/views/templates/script.blade.php
@@ -12,10 +12,22 @@
 <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
 <script src="{{ asset('/assets/demo/demo.js') }}"></script>
 <script src="{{ asset('/tagsinput/bootstrap-tagsinput.js') }}"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="https://cdn.tiny.cloud/1/vkg9qm33hdnpip24nh95a4y6z0xiufpig50atl7ngmwv4p2x/tinymce/5/tinymce.min.js"
-referrerpolicy="origin"></script>
+@production
+    <script src="{{ secure_asset('/assets/js/core/jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+    <!-- Chart JS -->
+    <script src="{{ secure_asset('/assets/js/plugins/chartjs.min.js') }}"></script>
+    <!--  Notifications Plugin    -->
+    <script src="{{ secure_asset('/assets/js/plugins/bootstrap-notify.js') }}"></script>
+    <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ secure_asset('/assets/js/now-ui-dashboard.min.js?v=1.5.0') }}" type="text/javascript"></script>
+    <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+    <script src="{{ secure_asset('/assets/demo/demo.js') }}"></script>
+    <script src="{{ secure_asset('/tagsinput/bootstrap-tagsinput.js') }}"></script>
+@endproduction
+
 <script>
     $(document).ready(function() {
         // Javascript method's body can be found in assets/js/demos.js
